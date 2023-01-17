505ac972a3444a833cecda94e0a6f732e7f7baf2
astegaru <andrei.stegaru@ionos.com>
2023-01-17 21:07:55 +0100

    detect wifi clients on network

---
+++ py-utils/detect_client_on_wifi.py
@@ -0,0 +1,20 @@
+import os
+from time import sleep
+
+
+# client counter
+number_clients = 0
+
+# first scan, initial number
+initial_scan = int(os.popen("nmap -sP 192.168.1.0/24 | grep 192 | wc -l").read())
+print(f'Initial number of clients: {initial_scan}')
+
+# run the program:
+while True:
+    scan = int(os.popen("nmap -sP 192.168.1.0/24 | grep 192 | wc -l").read())
+    
+    # if new client number change, notify me!
+    if number_clients != scan:
+        print(f'Number of clients changed: {scan}')
+        number_clients = scan
+        sleep(60)
