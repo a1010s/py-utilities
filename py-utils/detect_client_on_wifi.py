e8bc64e0720c0d300827b69b80f79e0fb7767825
astegaru <andrei.stegaru@ionos.com>
2023-01-17 21:19:18 +0100

    comments

--- py-utils/detect_client_on_wifi.py
+++ py-utils/detect_client_on_wifi.py
@@ -3,18 +3,18 @@ from time import sleep
 
 
 # client counter
-number_clients = 0
+number_clients = 0 
 
 # first scan, initial number
-initial_scan = int(os.popen("nmap -sP 192.168.1.0/24 | grep 192 | wc -l").read())
+initial_scan = int(os.popen("nmap -sP 192.168.1.0/24 | grep 192 | grep -v unifi.localdomain | wc -l").read())
 print(f'Initial number of clients: {initial_scan}')
 
 # run the program:
 while True:
-    scan = int(os.popen("nmap -sP 192.168.1.0/24 | grep 192 | wc -l").read())
+    scan = int(os.popen("nmap -sP 192.168.1.0/24 | grep 192 | grep -v unifi.localdomain | wc -l").read())
     
     # if new client number change, notify me!
     if number_clients != scan:
         print(f'Number of clients changed: {scan}')
         number_clients = scan
-        sleep(60)
+        sleep(10)
