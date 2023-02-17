38a6c20ac8b46d9fea2a95cafd2f38a3d61ce7d1
astegaru <andrei.stegaru@ionos.com>
2023-02-17 11:59:30 +0100

    improvements

--- py-k8s/k8s_log-monitor.py
+++ py-k8s/k8s_log-monitor.py
@@ -13,19 +13,19 @@ w = watch.Watch()
 
 for event in w.stream(api_instance.list_pod_for_all_namespaces):
     
-    # Print the type and name of the event
-    print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+    # Print the type, name, and namespace of the event
+    print("Event: %s %s in namespace %s" % (event['type'], event['object'].metadata.name, event['object'].metadata.namespace))
     
     # Filter events related to failed pods
     if 'failed' in event['object'].status.phase:
         
         # Print the name and reason of the failed pod
-        print("Pod: %s - Reason: %s" % (event['object'].metadata.name, event['object'].status.reason))
+        print("Pod: %s in namespace %s - Reason: %s" % (event['object'].metadata.name, event['object'].metadata.namespace, event['object'].status.reason))
         
         # Send OS notification
         notification.notify(
             title='Failed Pod',
-            message=f'Pod {event["object"].metadata.name} has failed. Reason: {event["object"].status.reason}',
+            message=f'Pod {event["object"].metadata.name} in namespace {event["object"].metadata.namespace} has failed. Reason: {event["object"].status.reason}',
             timeout=10
         )    
     else:
@@ -34,16 +34,16 @@ for event in w.stream(api_instance.list_pod_for_all_namespaces):
         if event['object'].status.container_statuses is not None:
             for container_status in event['object'].status.container_statuses:
                 if container_status.state.waiting is not None and 'ErrImagePull' in container_status.state.waiting.reason:
-                    print(f"Pod: {event['object'].metadata.name} - Reason: {container_status.state.waiting.reason}")
+                    print(f"Pod: {event['object'].metadata.name} in namespace {event['object'].metadata.namespace} - Reason: {container_status.state.waiting.reason}")
                     notification.notify(
                         title='Failed Pod',
-                        message=f'Pod {event["object"].metadata.name} has failed. Reason: {container_status.state.waiting.reason}',
+                        message=f'Pod {event["object"].metadata.name} in namespace {event["object"].metadata.namespace} has failed. Reason: {container_status.state.waiting.reason}',
                         timeout=10
                     )
                 elif container_status.state.terminated is not None and container_status.state.terminated.exit_code != 0:
-                    print(f"Pod: {event['object'].metadata.name} - Reason: {container_status.state.terminated.reason}")
+                    print(f"Pod: {event['object'].metadata.name} in namespace {event['object'].metadata.namespace} - Reason: {container_status.state.terminated.reason}")
                     notification.notify(
                         title='Failed Pod',
-                        message=f'Pod {event["object"].metadata.name} has failed. Reason: {container_status.state.terminated.reason}',
+                        message=f'Pod {event["object"].metadata.name} in namespace {event["object"].metadata.namespace} has failed. Reason: {container_status.state.terminated.reason}',
                         timeout=10
                     )
